452359a28886016a7329c77c4df199053dc735f7
Ufhano Rehoboth Tshivhidzo <101869965+Ufhano@users.noreply.github.com>
2025-12-29 01:21:35 +0200

    #12 Fix Render port , cleanup dotenv

--- server/index.js
+++ server/index.js
@@ -2,11 +2,8 @@ import 'dotenv/config';
 import express from 'express';
 import mongoose from 'mongoose';
 import cors from 'cors';
-import dotenv from 'dotenv';
 
 import chatRoutes from './routes/chat.routes.js';
-
-dotenv.config();
 
 const app = express();
 
@@ -24,6 +21,7 @@ app.get('/health', (_, res) => {
 
 app.use('/api/chat', chatRoutes);
 
-app.listen(5000, () => {
-  console.log('Backend is running ✔️');
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Backend is running on port ${PORT} ✔️`);
 });
